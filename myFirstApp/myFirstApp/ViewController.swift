9a180480de05ea3259c07f24b7babb83b87068fb
nantachai <bz.nantachai@gmail.com>
2023-09-17 15:03:18 +0700

    Introduction to iOS app development

---
+++ myFirstApp/myFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Nantachai Sangadsri on 9/9/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
