403ceac4e653ac3f447aa1d30b02877989121aea
nantachai <bz.nantachai@gmail.com>
2023-10-07 15:02:43 +0700

    Learn static table view .

---
+++ BTV01/BTV01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BTV01
+//
+//  Created by Nantachai Sangadsri on 7/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
